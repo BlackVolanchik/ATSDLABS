c498d0507415eff14eed65417bea2dca9d1ce4c9
BlackVolanchik <vovaprilipko340@gmail.com>
2025-03-12 21:49:59 +0200

    lab1

---
+++ atsdfull2/LinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atsdfull2
+{
+    class Node
+    {
+        public int Data;
+        public Node Next;
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    class LinkedList
+    {
+        public Node head;
+
+        public LinkedList()
+        {
+            head = null;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        public void AddToHead(int number)
+        {
+            Node newNode = new Node(number);
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        public int Remove(int index)
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Список порожній!");
+            }
+
+            if (index == 0)
+            {
+                int removedData = head.Data;
+                head = head.Next;
+                return removedData;
+            }
+
+            Node current = head;
+            Node prev = null;
+            int count = 0;
+
+            while (current != null && count < index)
+            {
+                prev = current;
+                current = current.Next;
+                count++;
+            }
+
+            prev.Next = current.Next;
+            return current.Data;
+        }
+
+        public void Print()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.Data + " -> ");
+                current = current.Next;
+            }
+            Console.WriteLine("null");
+        }
+    }
+}
---
+++ atsdfull2/LinkedListToStack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atsdfull2
+{
+
+    class LinkedListToStack
+    {
+        private LinkedList list = new LinkedList();
+
+        public void Add(int data)
+        {
+            list.AddToHead(data);
+        }
+
+        public void RemoveNegative()
+        {
+            int index = 0;
+
+            Node current = list.head;
+            while (current != null)
+            {
+                Node next = current.Next;
+
+                if (current.Data < 0)
+                {
+                    list.Remove(index);
+
+                }
+                else
+                {
+                    index++;
+                }
+
+                current = next;
+            }
+        }
+
+
+
+
+        public Stack<string> ConvertToStack()
+        {
+            Stack<string> stack = new Stack<string>();
+            Node current = list.head;
+
+            while (current != null)
+            {
+                stack.Push(Convert.ToString(current.Data, 8));
+                current = current.Next;
+            }
+
+            return stack;
+        }
+
+        public void Print()
+        {
+            list.Print();
+        }
+    }
+
+}
